12948f9bb443ee4270a49e085d9b9e5f88734591
Antikore <koopatropa8@hotmail.com>
2022-05-02 13:04:05 +0200

    Added the script to main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+# File 'main.py'
+# This file will run almost all the rest of scripts.
+
 import words
 import all
 
-all.Run()
-words.Run()
+# Perform the execution of scripts
+def Run():
+	all.Run()
+	words.Run()
+
+if (__name__ == '__main__'):
+	Run()
