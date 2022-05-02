860d45994918380c248f667e320bea6185834755
Antikore <koopatropa8@hotmail.com>
2022-05-02 13:04:20 +0200

    Added the script to all.py

---
+++ all.py
@@ -0,0 +1,26 @@
+# File 'all.py'
+# This file will extract all entries from lexicon/types and put it all together into 'all.txt'
+
+import parse
+import os
+
+# Run the script
+def Run():
+	
+	entries = []
+	directory = r"./lexicon/types"
+
+	for root, directories, files in os.walk(directory):
+		for file in files:
+			if (file.endswith(".txt")):
+				entries.extend(parse.Parse(os.path.join(root, file), False))
+
+	entries.sort()
+	
+	file = open('./lexicon/all.txt', 'w', encoding = 'utf-8')
+	file.write('\n'.join(entries))
+	file.close()
+	
+# Run the code if it was executed by console directly!
+if (__name__ == '__main__'):
+	Run()
