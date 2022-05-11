e64fd184a9b89ba0a67b06b8904200873d7e45d4
Antikore <koopatropa8@hotmail.com>
2022-05-11 11:42:20 +0200

    Added a gender generator

---
+++ genders.py
@@ -0,0 +1,71 @@
+# FIle 'genders.py'
+# This script will get all entries from lexicon/types, then make a list of all aidanish words not repeating any, then move it to words.txt
+
+import parse, sys
+
+female = ["a", "i", "s"]
+male = ["o", "u", "k"]
+
+# Run the script
+def Run():
+	
+	entries = parse.Parse('./lexicon/types/nouns.txt')
+	entries.extend(parse.Parse("./lexicon/types/adjectives.txt"))
+	words = []
+
+	entries = entries if len(sys.argv) < 2 else [ " → " + sys.argv[1] ]
+	inputted = len(sys.argv) >= 2
+
+	for entry in entries:
+		obj = entry.split(" → ")
+		if (len(obj) <= 1):
+			continue
+
+		trans = obj[1].split(", ")
+
+		for word in trans:
+			word = word.lower().strip()
+			if (" " in word):
+				continue
+			
+			end = word[len(word) -1]
+			if (end in male):
+				words.append("ei {}".format(word))
+			elif (end in female):
+				words.append("la {}".format(word))
+			else:
+				male_pred = 0
+				female_pred = 0
+				special = 0
+				for c in word:
+					if (c in "ou"):
+						male_pred += 1
+					elif (c in "a"):
+						female_pred += 1
+					elif (c in "i"):
+						special += 1
+				if (male_pred > female_pred):
+					words.append("ei {}".format(word))
+				elif (male_pred == female_pred):
+					if (end in "e"):
+						if (special > male_pred and special > female_pred):
+							words.append("ei {}".format(word))
+						else:
+							words.append("la {}".format(word))
+					else:
+						words.append("ei {}".format(word))
+				else:
+					words.append("la {}".format(word))
+
+	if (inputted):
+		print(words[0])
+		return
+
+
+	f = open('./lexicon/genders.txt', 'w', encoding = 'utf-8')
+	f.write('\n'.join(words))
+	f.close()
+		
+# Run the code if it was executed by console directly!
+if (__name__ == '__main__'):
+	Run()
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # File 'main.py'
 # This file will run almost all the rest of scripts.
 
-import words, done, all
+import words, done, all, genders
 
 # Perform the execution of scripts
 def Run():
 	all.Run()
 	words.Run()
 	done.Run()
+	genders.Run()
 
 if (__name__ == '__main__'):
 	Run()
