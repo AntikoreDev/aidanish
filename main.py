1fc3fff83b0a5263f554d90509715a8858efbd73
Antikore <koopatropa8@hotmail.com>
2022-05-11 09:12:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # File 'main.py'
 # This file will run almost all the rest of scripts.
 
-import words
-import all
+import words, done, all
 
 # Perform the execution of scripts
 def Run():
 	all.Run()
 	words.Run()
+	done.Run()
 
 if (__name__ == '__main__'):
 	Run()
