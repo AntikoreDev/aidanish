24614e47b75ca586d7fa9e56c4c8d5da00947c9e
Antikore <koopatropa8@hotmail.com>
2023-05-03 11:12:03 +0200

    Included conjugated.py and join.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # File 'main.py'
 # This file will run almost all the rest of scripts.
  
-from scripts import words, done, all, genders, jsondict
+from scripts import words, done, all, genders, jsondict, verbs, join
 
 # Perform the execution of scripts
 def Run():
@@ -10,6 +10,8 @@ def Run():
 	done.Run()
 	genders.Run()
 	jsondict.Run()
+	verbs.Run()
+	join.Run()
 
 
 if (__name__ == '__main__'):
